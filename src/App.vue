8a339fd252ae31681fef3350bc2c179fa0562598
zhangtuo <13120409722@163.com>
2022-03-20 15:34:23 +0800

    1.删除自带的 HelloWorld 组件；2.添加 Child 组件；3.将 Child 添加到 App 中

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Child></Child>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Child from "@/components/Child";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Child
+
   }
 }
 </script>
---
+++ src/components/Child.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child"
+}
+</script>
+
+<style scoped>
+
+</style>
